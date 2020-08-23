122c5ef656f17150866d565eecb7334514f6506d
Kate Cox <hello@kaitrin.dev>
2020-08-23 11:20:32 -0400

    Module 2.1-2.2 Instantiate state and pass props to OptionsPanel

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
 
 import './App.css';
 
-class App extends Component{
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Turbo-Matcher
-      </header>
-        <OptionsPanel />
-        <Board />
-      }
-    </div>
-  );
+        this.state = {
+            numTiles: 36,
+            playing: false,
+            previousTileIndex: null,
+            tiles: [],
+            toBeCleared: null
+        };
+    }
 
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Turbo-Matcher
+                </header>
+                <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} />
+                <Board/>
+                }
+            </div>
+        );
+
+    }
 }
 
 export default App;
